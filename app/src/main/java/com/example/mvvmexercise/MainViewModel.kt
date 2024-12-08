783a0a67793d72716112e8dd3f112a097a652074
Maciej Pawłowski <maciekpawlowski1@gmail.com>
2024-12-08 16:52:24 +0100

    Add solution

--- app/src/main/java/com/example/mvvmexercise/MainViewModel.kt
+++ app/src/main/java/com/example/mvvmexercise/MainViewModel.kt
@@ -1,22 +1,44 @@
 package com.example.mvvmexercise
 
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import kotlinx.coroutines.Job
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.launch
+import kotlin.time.Duration.Companion.seconds
 
 class MainViewModel :
     ViewModel(),
     IMainViewModel {
-    override val time: StateFlow<Int>
-        get() = TODO("Not yet implemented")
+    private val _time = MutableStateFlow(0)
+    private val _isRunning = MutableStateFlow(false)
 
-    override val isRunning: StateFlow<Boolean>
-        get() = TODO("Not yet implemented")
+    override val time: StateFlow<Int> = _time.asStateFlow()
+
+    override val isRunning: StateFlow<Boolean> = _isRunning.asStateFlow()
+
+    private var job: Job? = null
 
     override fun start() {
-        TODO("Not yet implemented")
+        viewModelScope.launch {
+            if (!isRunning.value) {
+                _isRunning.value = true
+                job =
+                    launch {
+                        while (isRunning.value) {
+                            _time.value++
+                            delay(1.seconds)
+                        }
+                    }
+            }
+        }
     }
 
     override fun stop() {
-        TODO("Not yet implemented")
+        job?.cancel()
+        _isRunning.value = false
     }
 }
